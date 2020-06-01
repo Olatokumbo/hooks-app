bacfa0b2017afc7f15486e707505fa6eb0ce2c9b
David Odesola <faithodesola@gmail.com>
2020-06-02 04:28:19 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+    const [notes, setNotes]= useState([]);
+    const [title, setTitle] = useState("");
+    const [body, setBody] =useState("");
+
+    const addNotes =(e) =>{
+        e.preventDefault();
+        setNotes([
+            ...notes,
+            {title, body}
+        ]);
+        setBody(" ")
+        setTitle(" ")
+     }
+    const removeNotes =(title) =>{
+        setNotes(notes.filter((notes)=>(notes.title !==title)))
+    }
+    return(
+        <div>
+        <h1>To Do App</h1>
+        <form onSubmit={addNotes}>
+        {notes.map((data) =>(
+            <div>
+            <h1>{data.title}</h1>
+            <p>{data.body}</p>
+            <button onClick={() =>removeNotes(data.title)}>X</button>
+            </div>
+        ))}
+        <input value={title} placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
+        <textarea  value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button>Add Notes</button>
+        </form>
+        </div>
+    );
+};
 
 export default App;
+
+
+
